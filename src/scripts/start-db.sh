2c29e7a6ac4eb52a49f6e9e33625896a47238976
Žan Pukmajster <45610204+pukmajster@users.noreply.github.com>
2022-02-28 17:49:51 +0100

    Add startup-db script

---
+++ src/scripts/start-db.sh
@@ -0,0 +1,24 @@
+
+# https://medium.com/@gausmann.simon/nestjs-typeorm-and-postgresql-full-example-development-and-project-setup-working-with-database-c1a2b1b11b8f
+#!/bin/bash
+set -e
+
+SERVER="quotes-server";
+PW="root";
+DB="quotes-db";
+
+echo "echo stop & remove old docker [$SERVER] and starting new fresh instance of [$SERVER]"
+(docker kill $SERVER || :) && \
+  (docker rm $SERVER || :) && \
+  docker run --name $SERVER -e POSTGRES_PASSWORD=$PW \
+  -e PGPASSWORD=$PW \
+  -p 5432:5432 \
+  -d postgres
+
+# wait for pg to start
+echo "sleep wait for pg-server [$SERVER] to start";
+SLEEP 3;
+
+# create the db 
+echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | docker exec -i $SERVER psql -U postgres
+echo "\l" | docker exec -i $SERVER psql -U postgres
