5a6215e3bae7dd16beb2f714a08fb4a101ecfe0a
Žan Pukmajster <45610204+pukmajster@users.noreply.github.com>
2022-03-01 14:13:28 +0100

    Configure db scripts

--- src/scripts/start-db copy.sh
+++ src/scripts/start-db copy.sh
@@ -6,20 +6,20 @@ set -e
 SERVER="quotes-dev";
 PW="root";
 DB="quotes-db";
-
+SLEEP="/bin/sleep";
 
 echo "echo stop & remove old docker [$SERVER] and starting new fresh instance of [$SERVER]"
-(docker kill $SERVER || :) && \
-  (docker rm $SERVER || :) && \
-  docker run --name $SERVER -e POSTGRES_PASSWORD=$PW \
+(sudo docker kill $SERVER || :) && \
+  (sudo docker rm $SERVER || :) && \
+  sudo docker run --name $SERVER -e POSTGRES_PASSWORD=$PW \
   -e PGPASSWORD=$PW \
-  -p 5432:5432 \
+  -p 5433:5433 \
   -d postgres
 
 # wait for pg to start
 echo "sleep wait for pg-server [$SERVER] to start";
-SLEEP 3;
+$SLEEP 3;
 
 # create the db 
-echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | docker exec -i $SERVER psql -U postgres
-echo "\l" | docker exec -i $SERVER psql -U postgres
+echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | sudo docker exec -i $SERVER psql -U postgres
+echo "\l" | sudo docker exec -i $SERVER psql -U postgres
