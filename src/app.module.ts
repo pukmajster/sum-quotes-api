db1ea4bdc180d829ca273e4f9d90d68113888edb
Žan Pukmajster <45610204+pukmajster@users.noreply.github.com>
2022-04-01 19:29:14 +0200

    Install Joi for db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
@@ -7,12 +8,23 @@ import { AuthModule } from './auth/auth.module';
 import { configService } from './config/config.service';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
+import * as Joi from "@hapi/joi";
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
