1d76562d945b12b12b8a852ffcf8ebb73cebd8bf
Žan Pukmajster <45610204+pukmajster@users.noreply.github.com>
2022-04-01 19:30:53 +0200

    Update JwtModule registration to use .env properties

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,14 +6,22 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
+        },
+      }),
     }),
   ],
   providers: [AuthService],
