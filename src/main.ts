558c3bc736939d4b56fd8c1510e5021e7ac9481c
Žan Pukmajster <45610204+pukmajster@users.noreply.github.com>
2022-04-04 17:46:17 +0200

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
-  await app.listen(3000);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+  await app.listen(8000);
 }
 bootstrap();
